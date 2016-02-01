bd799c67b77dfd36a035a89c90c27f2040c1b366
dahliacreative <simon@dahliacreative.com>
2016-02-01 08:55:05 +0000

    Scroll Position Fix

--- src/javascripts/selectron.updateScrollPosition.js
+++ src/javascripts/selectron.updateScrollPosition.js
@@ -6,12 +6,13 @@ Selectron.prototype.updateScrollPosition = function(hovered) {
       optionTop = hovered.position().top,
       optionHeight = hovered.outerHeight(),
       scrollPosition = this.options.scrollTop();
-
   if(hovered.is(':first-child')) {
       this.options.scrollTop(0);
   } else if(hovered.is(':last-child')) {
       this.options.scrollTop(this.options[0].scrollHeight);
-  } else if(optionTop > (listHeight - optionHeight)) {
+  } else if(((optionTop + optionHeight) - scrollPosition) > (listHeight)) {
       this.options.scrollTop(optionTop - (listHeight - optionHeight));
+  } else if((optionTop - scrollPosition) < 0) {
+    this.options.scrollTop(optionTop);
   } 
 }
