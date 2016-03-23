770a7b6043828a862da802becaaf37ed4826f1bf
dahliacreative <simon@dahliacreative.com>
2016-03-23 15:17:57 +0000

    Fixing issue where event listeners attached to selects before selectron init werem't firing

--- src/javascripts/selectron.build.js
+++ src/javascripts/selectron.build.js
@@ -3,25 +3,23 @@
 // --------------------------------------------------------------------------
 Selectron.prototype.build = function() {
   var wrapperClasses = this.select.attr('class');
-  this.wrapper = $('<div/>', { 'class': 'selectron' });
+
+  this.select
+    .removeAttr('class')
+    .addClass('selectron__select')
+    .wrap('<div class="selectron"/>');
+
+  this.wrapper = this.select.parent('.selectron');
   this.wrapper
     .addClass(wrapperClasses)
     .toggleClass('selectron--disabled', this.isDisabled)
     .toggleClass('selectron--is-touch', this.isTouch);
 
-  this.select
-    .removeAttr('class')
-    .addClass('selectron__select');
-
-  this.select.replaceWith(this.wrapper);
-
-  if(this.isTouch) {
-    this.wrapper.append(this.select);
-  } else {
+  if(!this.isTouch) {
     this.searchTerm = '';
     this.trigger = $('<button/>', { 'class': 'selectron__trigger', 'type': 'button' });
     this.options = $('<ul/>', { 'class': 'selectron__options' });
-    this.wrapper.append(this.select, this.trigger, this.options);
+    this.wrapper.append(this.trigger, this.options);
     this.registerEvents();
     this.populateOptions();
   }
